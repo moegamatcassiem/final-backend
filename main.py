f50d705ccd9eed7331ddca102dfcf66977b01bc2
moegamatcassiem <dallascassiem704@gmail.com>
2021-09-07 13:24:36 +0200

    updated

--- main.py
+++ main.py
@@ -118,10 +118,10 @@ def user_registration():
     response = {}
     if request.method == "POST":
         with sqlite3.connect("e-store.db") as conn:
-            firstname = request.json['firstname']
-            surname = request.json['surname']
-            email = request.json['email']
-            password = request.json['password']
+            firstname = request.form['firstname']
+            surname = request.form['surname']
+            email = request.form['email']
+            password = request.form['password']
             cursor = conn.cursor()
             cursor.execute("INSERT INTO users (firstname, surname, email, password) VALUES(?, ?, ?, ?)",
                            (firstname, surname, email, password))
